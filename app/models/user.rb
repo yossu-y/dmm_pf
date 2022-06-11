c0ba19bef0e78b1efc430c687d1567863f140903
yossu-y <yangyuub@gmail.com>
2022-06-11 13:40:47 +0000

    [update]relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,27 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
 
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
   def get_profile_image
     (profile_image.attached?)? profile_image: "no-image-icon.jpg"
+  end
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 
 
