acfb4385c6078baaf2fd3abb2bbc30853a5a4090
yossu-y <yangyuub@gmail.com>
2022-06-11 11:09:15 +0000

    [update]users-view.controller

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,9 +1,11 @@
 class Public::UsersController < ApplicationController
-
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
   before_action :ensure_guest_user, only: [:edit]
 
   def index
     @users = User.all
+    @user = User.find(params[:id])
   end
 
   def show
@@ -14,15 +16,37 @@ class Public::UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを更新しました！"
+    else
+      render "edit"
+    end
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path, notice: "退会処理が完了しました。"
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:screen_name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
 
   def ensure_guest_user
     @user = User.find(params[:id])
