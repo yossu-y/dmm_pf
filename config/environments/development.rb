2a262e5ab923bc60575a547e06fe8cbf3abaeeb8
yossu-y <yangyuub@gmail.com>
2022-06-11 05:11:30 +0000

    [add]activestorage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "60ad5f5138604381946d1efb3db64b1f.vfs.cloud9.us-east-1.amazonaws.com"
-  
+  config.active_job.queue_adapter = :inline
+
 end
