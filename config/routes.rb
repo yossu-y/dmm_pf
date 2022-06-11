5e2c7051be4c29affdf766463437b4cdc89266da
yossu-y <yangyuub@gmail.com>
2022-06-11 11:41:15 +0000

    [update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
       resource :like, only: [:create, :destroy]
     end
 
-    get "users/:id/likes" => "users#likes", as: "user_like"
     get "users/unsubscribe/:id" => "users#unsubscribe", as: "unsubscribe"
     patch "users/withdraw" => "users#withdraw"
     resources :users, only: [:edit, :update, :show] do
