9fba33abab04bc66df2d03db992c2ac034cc1a18
yossu-y <yangyuub@gmail.com>
2022-06-11 08:35:54 +0000

    [update]devise

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -23,9 +23,8 @@ class Admin::SessionsController < Devise::SessionsController
     admin_users_path
   end
 
-  # 管理者のログアウトは不要なので最終消す
   def after_sign_out_path_for(resource)
-    new_admin_session_path
+    root_path
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions'
+    sessions: "public/sessions"
   }
 
   scope module: :public do
@@ -40,9 +40,9 @@ Rails.application.routes.draw do
     resources :contacts, only: [:create, :new]
 
   end
-  
+
   # ゲストログイン用
-  
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
