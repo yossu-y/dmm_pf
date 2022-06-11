0fd5467abd3976513c4f0f0ca8acf6724454afed
yossu-y <yangyuub@gmail.com>
2022-06-11 13:10:26 +0000

    [update]views/articles

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -1,27 +1,58 @@
 class Public::ArticlesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
+    @articles = Article.all.order(created_at: :desc)
   end
 
   def new
+    @article = Article.new
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
+    @article = current_user.articles.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      @articles = Article.all
+      render "new"
+    end
   end
 
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
-  def user_index
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :is_draft, :image)
+  end
+
+  def correct_user
+    @article = Article.find(params[:id])
+    @user = @article.user
+    redirect_to(articles_path) unless @user == current_user
   end
 
 
