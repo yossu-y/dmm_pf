82c8f07e866e6728a67e968bbc30d30590f211fb
yossu-y <yangyuub@gmail.com>
2022-06-12 04:10:07 +0000

    [update]like.comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,13 +5,11 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.article_id = article.id
     @comment.save
-    redirect_to request.referer
   end
 
   def destroy
-    @commnet = Comment.find_by(id: params[:id], article_id: params[:article_id])
+    @comment = Comment.find_by(id: params[:id], article_id: params[:article_id])
     @comment.destroy
-    redirect_to request.referer
   end
 
   private
--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -4,14 +4,12 @@ class Public::LikesController < ApplicationController
     @article = Article.find(params[:article_id])
     @like = current_user.likes.new(article_id: @article.id)
     @like.save
-    redirect_to request.referer
   end
 
   def destroy
     @article = Article.find(params[:article_id])
     @like = current_user.likes.find_by(article_id: @article.id)
     @like.destroy
-    redirect_to request.referer
   end
 
 end
