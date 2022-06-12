80299fffc1566062650f136f1693f37ee6a50fd3
yossu-y <yangyuub@gmail.com>
2022-06-12 07:14:45 +0000

    [update]views/users

---
+++ app/javascript/script.js
@@ -0,0 +1,13 @@
+
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    $('.tab').click(function(){
+      $('.tab-active').removeClass('tab-active');
+      $(this).addClass('tab-active');
+      $('.box-show').removeClass('box-show');
+      const index = $(this).index();
+      $('.tabbox').eq(index).addClass('box-show');
+    });
+  });
+});
+
