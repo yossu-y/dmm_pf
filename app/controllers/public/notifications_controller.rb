15bcce7377b50e0c81b5fc761f1860ede7b8978f
yossu-y <yangyuub@gmail.com>
2022-06-15 02:16:33 +0000

    [update]comments

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -4,7 +4,9 @@ class Public::NotificationsController < ApplicationController
     @notifications = current_user.passive_notifications.all
     # created_at: Time.zone.now.all_day←通知後24時間でindexから消え
     # checked: falseとtrueで確認済みか判断して通知マークを消す
-    @notifications.where(checked: false, created_at: Time.zone.now.all_day).each do |notification|
+    # @notifications.where(created_at: Time.zone.now.all_day)
+
+    @notifications.where(created_at: Time.zone.now.all_day, checked: false).each do |notification|
       notification.update(checked: true)
     end
 
