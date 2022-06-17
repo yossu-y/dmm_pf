5ecbf8eff1648b80b67b1975059a4f22cb0a7a5d
yossu-y <yangyuub@gmail.com>
2022-06-17 14:32:53 +0000

    [update]layouts

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -4,11 +4,7 @@ class Public::CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = current_user.comments.new(comment_params)
     @comment.article_id = @article.id
-    if @comment.save
-      flash.now[:alert] = "コメントしました"
-    else
-      flash.now[:alert] = "150文字以内で入力してください"
-    end
+    @comment.save
     # 自分の記事へのコメントは通知しない
     if @article.user_id != current_user.id
       @article.create_notification_comment!(current_user, @comment.id)
@@ -18,7 +14,6 @@ class Public::CommentsController < ApplicationController
   def destroy
     @comment = Comment.find_by(id: params[:id], article_id: params[:article_id])
     @comment.destroy
-      flash.now[:alert] = "コメントを削除しました"
   end
 
   private
