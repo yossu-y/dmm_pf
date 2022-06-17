c0bf1a2b323a7deb13200c612d059bd4767820c4
yossu-y <yangyuub@gmail.com>
2022-06-17 03:24:42 +0000

    [update]contact

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,14 +1,41 @@
 class Public::ContactsController < ApplicationController
 
   def new
+    @contact = Contact.new
   end
 
   def confirm
-  end
-
-  def thanks
+    @contact = Contact.new(contact_params)
+    if @contact.invalid?
+      render "new"
+    else
+      render "confirm"
+    end
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    @contact.user = contact_user
+    if @contact.save
+      # ContactMailer.send_mail(@contact).deliver_now
+      render "thanks"
+    else
+      render "new"
+    end
   end
+
+
+  def thanks
+    @contact = Contact.new(contact_params)
+    # ContactMailer.received_email(@contact).delicer
+    render "thanks"
+  end
+
+  private
+
+  def contact_params
+    params.permit(:name, :email, :subject, :message)
+  end
+
+
 end
