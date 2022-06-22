cc4b257f0a58caf55d9ef417e95c91b3e7f5a2c4
yossu-y <yangyuub@gmail.com>
2022-06-22 12:19:36 +0000

    update

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,6 +4,6 @@ class Like < ApplicationRecord
   belongs_to :article
   has_many :notifications, dependent: :destroy
 
-  validates_uniquness_of :article_id, scope: :user_id
+  validates_uniqueness_of :article_id, scope: :user_id
 
 end
