b045429130f14bd4e686d2ebc8180f1640ec458a
yossu-y <yangyuub@gmail.com>
2022-06-26 08:05:30 +0000

    update

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,7 +2,7 @@ class Public::NotificationsController < ApplicationController
 
   def index
     @notifications = current_user.passive_notifications.all
-    # 確認後24時間で通知が消える
+    # 送られた後24時間で通知が消える
     @notifications.where( created_at: Time.zone.now.all_day )
     # checked: falseとtrueで確認済みか判断して通知マークを消す
     # 確認した通知はtrueに変わって通知マークが消える
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -4,7 +4,7 @@ class Public::SearchesController < ApplicationController
     @range = params[:range]
 
     if @range == "ユーザー"
-      # whereを使って退会済みユーザーを表示
+      # whereを使って退会済みユーザーを非表示
       @users = User.where(is_deleted: false).search(params[:search], params[:keyword])
     elsif @range == "投稿"
       @articles = Article.search(params[:search], params[:keyword])
