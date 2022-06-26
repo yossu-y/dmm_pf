14f45b1f5ffd3b796c5fd11c5b390fabae71cda0
yossu-y <yangyuub@gmail.com>
2022-06-26 12:42:02 +0000

    update

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,14 +1,21 @@
 class Public::NotificationsController < ApplicationController
 
   def index
+
+    # 通知を確認後消すコード
+    # current_user.passive_notifications.where(checked: false).where("created_at <= ?", Time.now.ago(1.day)).update_all(checked: true)
+    # @notifications = current_user.passive_notifications.where(checked: false)
+    # viewファイルの最後に記載する<% @notifications.update_all(checked: true) %>
+
+    # 24時にリセットされるコード
     @notifications = current_user.passive_notifications.all
-    # 送られた後24時間で通知が消える
+    # 確認後24時間で通知が消える
     @notifications.where( created_at: Time.zone.now.all_day )
+    @notifications = @notifications.where( created_at: Time.zone.now.all_day )
     # checked: falseとtrueで確認済みか判断して通知マークを消す
-    # 確認した通知はtrueに変わって通知マークが消える
+    # 確認した通知はtrueに変わって消える
     @notifications.where( checked: false ).each do |notification|
       notification.update(checked: true)
     end
   end
-
 end
