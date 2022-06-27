32f648f70bc39a4224e10f6b6bf134c50c1f9469
yossu-y <yangyuub@gmail.com>
2022-06-27 15:53:49 +0000

    update

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -1,7 +1,7 @@
 module Public::NotificationsHelper
 
   def unchecked_notifications
-    @notifications = current_user.passive_notifications.where(checked: false)
+    @notifications = current_user.passive_notifications.where(checked: false, created_at: Time.zone.now.all_day)
   end
 
 end
