36cc959d87e1783972621d50201dc4d830b56dcb
yossu-y <yangyuub@gmail.com>
2022-07-06 08:06:54 +0000

    [update]layouts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require turbolinks
+//= require_tree .
+//= require social-share-button
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
