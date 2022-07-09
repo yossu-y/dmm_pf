47c3d4b1045ebc73dfc02cea8943893952784649
yossu-y <yangyuub@gmail.com>
2022-07-09 08:48:56 +0000

    [update]group-RSpec

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -7,7 +7,18 @@ require 'rails_helper'
 
       subject { test_group.valid? }
       let(:test_group) { group }
-
+      
+      centext "nameカラム" do
+        it "空欄でないこと" do
+          test_group.name = ""
+          is_expected.to eq true;
+        end
+        
+        it "50文字以下であること" do
+          group.name = Faker::Lorem.character(number:51)
+          expect(group.valid?).to eq true;
+        end
+      end
 
     end
 
