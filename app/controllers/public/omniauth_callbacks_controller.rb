44dd1ebb80e57da97aef601ad5b9324bd09962e2
yossu-y <yangyuub@gmail.com>
2022-07-12 06:26:27 +0000

    [finish]googleAPI

--- app/controllers/public/omniauth_callbacks_controller.rb
+++ app/controllers/public/omniauth_callbacks_controller.rb
@@ -11,6 +11,7 @@ class Public::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # 先ほどuser.rbで記述したメソッド(from_omniauth)をここで使っています
     # 'request.env["omniauth.auth"]'この中にgoogoleアカウントから取得したメールアドレスや、名前と言ったデータが含まれています
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    # byebug
     sign_in_and_redirect @user, event: :authentication
     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -75,7 +75,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       # deviseのuserカラムに name を追加している場合は以下のコメントアウトも追記します
       user.name = auth.info.name
-      user.screen_name = auth.info.screen_name
+      user.screen_name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
